637d347e26a3c6bf6358636e55795f0e851af083
sc30gsw <haidoushi875@gmail.com>
2023-07-19 08:24:23 +0900

    バリデーションの実装

--- src/task/task.resolver.ts
+++ src/task/task.resolver.ts
@@ -1,6 +1,7 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { TaskService } from './task.service'
 import { Task } from './models/task.modle'
+import { CreateTaskInput } from './dto/createTask.input'
 
 @Resolver()
 export class TaskResolver {
@@ -13,11 +14,7 @@ export class TaskResolver {
   }
 
   @Mutation(() => Task)
-  createTask(
-    @Args('name') name: string,
-    @Args('dueDate') dueDate: string,
-    @Args('description', { nullable: true }) description: string
-  ): Task {
-    return this.taskService.createTask(name, dueDate, description)
+  createTask(@Args('createTaskInput') createTaskInput: CreateTaskInput): Task {
+    return this.taskService.createTask(createTaskInput)
   }
 }
