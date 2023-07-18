61e0505cdb3f4f18db668321495a2ba4185c4965
sc30gsw <haidoushi875@gmail.com>
2023-07-19 07:41:47 +0900

    taskサービスの作成

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TaskResolver } from './task.resolver';
+import { TaskService } from './task.service';
 
 @Module({
-  providers: [TaskResolver]
+  providers: [TaskResolver, TaskService]
 })
 export class TaskModule {}
