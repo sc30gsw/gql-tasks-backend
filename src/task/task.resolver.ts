59460d927a806273fb5bef984a99672b228230a4
sc30gsw <haidoushi875@gmail.com>
2023-07-19 07:46:26 +0900

    Task一覧取得APIの作成

--- src/task/task.resolver.ts
+++ src/task/task.resolver.ts
@@ -1,4 +1,14 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql'
+import { TaskService } from './task.service'
+import { Task } from './models/task.modle'
 
 @Resolver()
-export class TaskResolver {}
+export class TaskResolver {
+  constructor(private readonly taskService: TaskService) {}
+
+  // 配列が存在しない場合、空配列を返す（nullable: 'items）
+  @Query(() => [Task], { nullable: 'items' })
+  getTasks(): Task[] {
+    return this.taskService.getTasks()
+  }
+}
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { Task } from './models/task.modle'
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  tasks: Task[] = []
+
+  getTasks(): Task[] {
+    const task1 = new Task()
+    task1.id = 1
+    task1.name = 'task1'
+    task1.dueDate = '2023-01-01'
+    task1.status = 'NOT_STARTED'
+    this.tasks.push(task1)
+
+    return this.tasks
+  }
+}
