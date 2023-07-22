c4e1435c9ebda07f72fc78f637655f1ab47b5e11
sc30gsw <haidoushi875@gmail.com>
2023-07-22 14:26:10 +0900

    User作成機能の実装

---
+++ src/user/user.resolver.ts
@@ -0,0 +1,17 @@
+import { Args, Mutation, Resolver } from '@nestjs/graphql'
+import { UserService } from './user.service'
+import { User as UserModel } from './models/user.model'
+import { CreateUserInput } from './dto/createUser.input'
+import { User } from '@prisma/client'
+
+@Resolver()
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @Mutation(() => UserModel)
+  async createUser(
+    @Args('createUserInput') createUserInput: CreateUserInput
+  ): Promise<User> {
+    return await this.userService.createUser(createUserInput)
+  }
+}
