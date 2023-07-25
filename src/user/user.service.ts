1975a70d1ce7e85fb3bf0aa5adbbcd58df0366ed
sc30gsw <haidoushi875@gmail.com>
2023-07-25 12:57:07 +0900

    UserServiceのテストコードの実装

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common'
-import { PrismaService } from 'src/prisma/prisma.service'
+import { PrismaService } from '../prisma/prisma.service'
 import { CreateUserInput } from './dto/createUser.input'
 import { User } from '@prisma/client'
 import * as bcrypt from 'bcrypt'
---
+++ src/user/user.test.spec.ts
@@ -0,0 +1,66 @@
+import { Test } from '@nestjs/testing'
+import { UserService } from './user.service'
+import { PrismaService } from '../prisma/prisma.service'
+import * as bcrypt from 'bcrypt'
+
+const MockPrismaService = () => ({
+  user: {
+    create: jest.fn(),
+    findUnique: jest.fn(),
+  },
+})
+
+describe('taskServiceTest', () => {
+  let userService
+  let prismaService
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        UserService,
+        { provide: PrismaService, useValue: MockPrismaService() },
+      ],
+    }).compile()
+
+    userService = module.get<UserService>(UserService)
+    prismaService = module.get<PrismaService>(PrismaService)
+  })
+
+  describe('createUser', () => {
+    it('正常系', async () => {
+      const expected = {
+        id: 1,
+        name: 'testUser1',
+        email: 'test1@example.com',
+        password: await bcrypt.hash('password', 10),
+      }
+
+      prismaService.user.create.mockResolvedValue(expected)
+      const result = await userService.createUser({
+        name: 'testUser1',
+        email: 'test1@example.com',
+        password: 'password',
+      })
+
+      expect(result.name).toEqual(expected.name)
+      expect(result.email).toEqual(expected.email)
+      expect(await bcrypt.compare('password', result.password)).toBe(true)
+    })
+  })
+
+  describe('getUser', () => {
+    it('正常系', async () => {
+      const expected = {
+        id: 1,
+        name: 'testUser1',
+        email: 'test1@example.com',
+      }
+
+      prismaService.user.findUnique.mockResolvedValue(expected)
+
+      const result = await userService.getUser('test1@example.com')
+
+      expect(result).toEqual(expected)
+    })
+  })
+})
